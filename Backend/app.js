4bd0ad440f10e97849f63cc5604fb7af7359ccf4
tacher-ki <rahul.20bccn029@jecrcu.edu.in>
2024-08-25 15:54:33 +0530

    dsf

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require('helmet');
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+
+const form =  require("./routes/viroutes");
+
+
+app.use("/api/v1", form);
+
+//ADF Fome Routers
+app.use("/api/v1", require("./routes/viroutes"));
+
+module.exports= app ;
+
+
